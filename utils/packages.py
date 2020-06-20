5e32f6f3b5220ac3e9f268d098e016ffd3ca4e34
Ramkumar Rajagopalan <ramrajag6@gmail.com>
2020-06-20 19:32:30 +0800

    streamlined file and folder structure

--- utils/packages.py
+++ utils/packages.py
@@ -1,35 +1,27 @@
 # Normal Packages
-
-import numpy as np
-import pandas as pd
-
-import yaml
-import json
-
 import sys
-import time
 import pytz
+import yaml
+import time
+import json
+import warnings
 import datetime
 import winsound
+import numpy as np
 import collections
-
+import pandas as pd
 from tqdm import tqdm
-
-import warnings
 warnings.filterwarnings('ignore')
 
 # Oanda Packages
-
 from oandapyV20 import API
-import oandapyV20.endpoints.orders as orders
 import oandapyV20.endpoints.trades as trades
+import oandapyV20.endpoints.orders as orders
 import oandapyV20.endpoints.pricing as pricing
 import oandapyV20.endpoints.accounts as accounts
 import oandapyV20.endpoints.transactions as trans
 import oandapyV20.endpoints.positions as positions
-
 import oandapyV20.definitions.pricing as defpricing
-
 import oandapyV20.endpoints.instruments as instruments
-
-from oandapyV20.contrib.requests import (MarketOrderRequest, StopLossDetails)
+from oandapyV20.contrib.requests import StopLossDetails
+from oandapyV20.contrib.requests import MarketOrderRequest
--- utils/tools.py
+++ utils/tools.py
@@ -1,6 +1,76 @@
 from utils.packages import *
 
 
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Terminate connection
+def terminate_connection():
+    try:
+        r.terminate(message = "maxrecs records received")
+    except:
+        pass
+#=============================================================================================================================================================================    
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Get timestamp of the price and segregate it
+def get_date_time(resp):
+    time_stamp = resp['time']
+    date_val, full_time = time_stamp.split(sep = 'T')
+    time_val, time_fraction = full_time.split(sep = '.')
+    return(date_val, time_val, time_fraction)
+#=============================================================================================================================================================================
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Get bid and ask prices
+def get_prices(resp):    
+    bid_price = float(resp['bids'][0]['price'])    
+    ask_price = float(resp['asks'][0]['price'])
+    spread = ask_price - bid_price
+    tick_price = (ask_price + bid_price) / 2
+    return(bid_price, ask_price, spread, tick_price)
+#=============================================================================================================================================================================
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+def calc_duration(start_time, end_time):        
+    seconds_elapsed = end_time - start_time
+    hours, rest = divmod(seconds_elapsed, 3600)
+    minutes, seconds = divmod(rest, 60)
+    duration = f'{round(hours)}:{round(minutes)}:{round(seconds)}'
+    return(duration)
+#=============================================================================================================================================================================
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+def get_loss_limits(min_trans_num, loss_limits):
+    dividing_val = 1
+    per_list = list(loss_limits.keys())    
+    for i, val in enumerate(per_list):
+        dividing_val = 100/val
+        loss_limits[val]['half_min_trans_num'] = (round((min_trans_num/dividing_val),0))*(-1)
+    return(loss_limits)
+#=============================================================================================================================================================================
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+def run_currency_num_check(accountID, currencies, iter_num, api):
+    for i, instrument in enumerate(currencies['currs']):        
+        pip_size = currencies['currs'][instrument]['pip_size']
+        pip_gap = currencies['currs'][instrument]['pip_gap']
+        num = get_min_trans_num(instrument, accountID, iter_num, pip_gap, pip_size, api)
+        print(f'instrument : {instrument}, num : {num}, pip_size : {pip_size}')
+    return()
+#=============================================================================================================================================================================
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def get_targets(start_price, target_num, pip_size):
     positive_targets = {}
     negative_targets = {}
@@ -11,9 +81,46 @@ def get_targets(start_price, target_num, pip_size):
         negative_targets[i] = start_price - move_val
         
     return(positive_targets, negative_targets)
+#=============================================================================================================================================================================
 
 
 
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+def make_order(accountID, stop_price, instrument, units, api):
+    stopLossOnFill = StopLossDetails(price=stop_price)
+
+    ordr = MarketOrderRequest(
+        instrument = instrument,
+        units=units,
+        stopLossOnFill=stopLossOnFill.data)
+
+    r = orders.OrderCreate(accountID, data=ordr.data)
+    rv = api.request(r)
+    return(rv)
+#=============================================================================================================================================================================
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+def close_order(accountID, order_type, instrument, api):
+    data_long = {"longUnits": "ALL"}
+    data_short = {"shortUnits": "ALL"}
+    
+    if order_type == 'long':
+        data = data_long
+    elif order_type == 'short':
+        data = data_short
+        
+    r = positions.PositionClose(accountID=accountID,
+                                instrument=instrument,
+                                data=data)
+    rv = api.request(r)
+    return(rv)
+#=============================================================================================================================================================================
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def get_direction(df_reached_targets, target_num,  min_count, pip_position, min_count_mulitplier):
     tot_pos = sum(df_reached_targets['positive'] * df_reached_targets['target_num'])
     tot_neg = sum(df_reached_targets['negative'] * df_reached_targets['target_num'])
@@ -28,19 +135,11 @@ def get_direction(df_reached_targets, target_num,  min_count, pip_position, min_
         direction = 'no_direction'
     
     return(direction)
+#=============================================================================================================================================================================
 
 
 
-def run_currency_num_check(accountID, currencies, iter_num, api):
-    for i, instrument in enumerate(currencies['currs']):        
-        pip_size = currencies['currs'][instrument]['pip_size']
-        pip_gap = currencies['currs'][instrument]['pip_gap']
-        num = get_min_trans_num(instrument, accountID, iter_num, pip_gap, pip_size, api)
-        print(f'instrument : {instrument}, num : {num}, pip_size : {pip_size}')
-    return()
-
-
-
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def get_min_trans_num(instrument,accountID,iter_num, pip_gap, pip_size, api):
     pip_gap = pip_gap * pip_size
     
@@ -86,89 +185,4 @@ def get_min_trans_num(instrument,accountID,iter_num, pip_gap, pip_size, api):
     #print(iter_req)
     min_trans = round(np.mean(iter_req),0)
     return(np.mean(min_trans))    
-
-
-
-def make_order(accountID, stop_price, instrument, units, api):
-    stopLossOnFill = StopLossDetails(price=stop_price)
-
-    ordr = MarketOrderRequest(
-        instrument = instrument,
-        units=units,
-        stopLossOnFill=stopLossOnFill.data)
-
-    r = orders.OrderCreate(accountID, data=ordr.data)
-    rv = api.request(r)
-    return(rv)
-
-
-
-def close_order(accountID, order_type, instrument, api):
-    data_long = {"longUnits": "ALL"}
-    data_short = {"shortUnits": "ALL"}
-    
-    if order_type == 'long':
-        data = data_long
-    elif order_type == 'short':
-        data = data_short
-        
-    r = positions.PositionClose(accountID=accountID,
-                                instrument=instrument,
-                                data=data)
-    rv = api.request(r)
-    return(rv)
-
-
-
-
-
-# Get timestamp of the price and segregate it
-def get_date_time(resp):
-    
-    time_stamp = resp['time']
-    date_val, full_time = time_stamp.split(sep = 'T')
-    time_val, time_fraction = full_time.split(sep = '.')
-    
-    return(date_val, time_val, time_fraction)
-
-
-
-# Get bid and ask prices
-def get_prices(resp):
-    
-    bid_price = float(resp['bids'][0]['price'])    
-    ask_price = float(resp['asks'][0]['price'])
-    spread = ask_price - bid_price
-    tick_price = (ask_price + bid_price) / 2
-    
-    return(bid_price, ask_price, spread, tick_price)
-
-
-
-def calc_duration(start_time, end_time):        
-    seconds_elapsed = end_time - start_time
-    hours, rest = divmod(seconds_elapsed, 3600)
-    minutes, seconds = divmod(rest, 60)
-    duration = f'{round(hours)}:{round(minutes)}:{round(seconds)}'
-    return(duration)
-
-
-
-
-def get_loss_limits(min_trans_num, loss_limits):
-    dividing_val = 1
-    per_list = list(loss_limits.keys())
-    
-    for i, val in enumerate(per_list):
-        dividing_val = 100/val
-        loss_limits[val]['half_min_trans_num'] = (round((min_trans_num/dividing_val),0))*(-1)
-    return(loss_limits)
-
-
-
-# Terminate connection
-def terminate_connection():
-    try:
-        r.terminate(message = "maxrecs records received")
-    except:
-        pass
+#=============================================================================================================================================================================
