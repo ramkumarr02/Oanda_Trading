c733b2f9dc0ddd0162dc80ff1104941f10300a94
Ramkumar Rajagopalan <ramrajag6@gmail.com>
2022-06-15 22:05:43 +0800

    latest commit

--- bt_multi_position/utils/variables.py
+++ bt_multi_position/utils/variables.py
@@ -131,13 +131,13 @@ data['loss_switch_pl_pip']      = -0.0010
 data['all_close_min_pip']      = 0.0000
 
 data["product"] = 'eurusd'
-data["input_year"] = 2021
+data["input_year"] = 2020
 data["input_file_name"] = f'{data["product"]}_{data["input_year"]}.csv'
 
-data['df_subset_size'] = 5000
+data['df_subset_size'] = None
 
-data['start_date'] = {'year':2021, 'month':1, 'date':1}
-data['end_date']   = {'year':2021, 'month':1, 'date':4}
+data['start_date'] = {'year':2020, 'month':1, 'date':1}
+data['end_date']   = {'year':2020, 'month':12, 'date':31}
 
 data['ema_roll_method'] = 'file'
 
