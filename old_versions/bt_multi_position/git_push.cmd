2cd0bfcbc33f6c34790f3c0a7a1cbe10bfec304c
Ramkumar Rajagopalan <ramrajag6@gmail.com>
2022-06-19 12:59:54 +0800

    changes

--- old_versions/bt_multi_position/git_push.cmd
+++
@@ -1,3 +0,0 @@
-git add . & 
-git commit -m "latest commit" &
-git push -u origin master
--- old_versions/bt_multi_position/runme.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt install screen
-yes | sudo apt install python3-pip
-cd bt_3ema_trader
-pip3 install -r requirements.txt
-screen -S myscreen
