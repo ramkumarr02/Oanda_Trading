d8e171cb5d1d77209994a4d5131c56f129a97d89
Ramkumar Rajagopalan <ramrajag6@gmail.com>
2022-07-19 14:25:33 +0800

    latest commit

---
+++ bt_angle_multi/utils/order.py
@@ -0,0 +1,316 @@
+from utils.packages import *
+from utils.i_o import *
+from utils.dir_slope import *
+
+
+
+#...............................................................................................
+def make_order(data):
+    if not data['open_order']:
+        if data['to_order'] == 'long':
+            data = make_long_order(data)
+
+        elif data['to_order'] == 'short':
+            data = make_short_order(data)
+
+        elif data['reverse'] == 'long':
+            data = make_long_order(data)
+            data['reverse'] = 'reversed'
+
+        elif data['reverse'] == 'short':
+            data = make_short_order(data)
+            data['reverse'] = 'reversed'
+
+    return(data)
+#...............................................................................................
+
+#...............................................................................................
+def calculate_pl(data):
+    if data['open_order_type'] == 'long':
+        data['close_bid_price'] = data['bid']
+        data['pl'] = np.round(data['close_bid_price'] - data['order_ask_price'], 5)
+
+    if data['open_order_type'] == 'short':
+        data['close_ask_price'] = data['ask']
+        data['pl'] = np.round(data['order_bid_price'] - data['close_ask_price'], 5)
+
+    return(data) 
+#...............................................................................................
+
+# ...............................................................................................
+def loss_reverse_position(data):   
+    data['stop_loss_pip']               = min(data['min_stop_loss_pip'], -data['h_l_gap'] * data['stop_loss_multiplier'])
+
+    if data['reverse'] == None:
+        if data['open_order']:
+            if data['pl'] <= data['stop_loss_pip']:
+                if data['open_order_type'] == 'long':
+                    if data['tick_angle'] < -data['min_order_angle']:
+                        data['stop_text'] = 'reversed'
+                        data = close_long_order(data)
+                        data['reverse'] = 'short'
+                        
+                if data['open_order_type'] == 'short':       
+                    if data['tick_angle'] > data['min_order_angle']:         
+                        data['stop_text'] = 'reversed'
+                        data = close_short_order(data)            
+                        data['reverse'] = 'long'  
+
+    elif data['reverse'] != None:
+        if data['open_order_type'] == 'long':
+            if data['pl'] <= data['stop_loss_pip'] * 2:
+                data['stop_text'] = 'simple_stop'
+                data = close_long_order(data)
+
+        if data['open_order_type'] == 'short':       
+            if data['pl'] <= data['stop_loss_pip'] * 2:
+                data['stop_text'] = 'simple_stop'
+                data = close_short_order(data)
+
+    return(data)   
+# ...............................................................................................   
+
+# ...............................................................................................
+def loss_reverse_position_continous(data):   
+    data['stop_loss_pip']               = min(data['min_stop_loss_pip'], -data['h_l_gap'] * data['stop_loss_multiplier'])
+
+    if data['open_order']:
+        if data['pl'] <= data['stop_loss_pip']:
+            if data['open_order_type'] == 'long':
+                if data['tick_angle'] < -data['min_order_angle']:
+                    data['stop_text'] = 'reversed'
+                    data = close_long_order(data)
+                    data['reverse'] = 'short'
+                else:
+                    if data['pl'] <= data['stop_loss_pip'] * 2:
+                        data['stop_text'] = 'simple_stop'
+                        data = close_long_order(data)
+
+                    
+            if data['open_order_type'] == 'short':       
+                if data['tick_angle'] > data['min_order_angle']:         
+                    data['stop_text'] = 'reversed'
+                    data = close_short_order(data)            
+                    data['reverse'] = 'long' 
+                else:
+                    if data['pl'] <= data['stop_loss_pip'] * 2:
+                        data['stop_text'] = 'simple_stop'
+                        data = close_short_order(data)
+    
+    return(data)   
+# ...............................................................................................   
+
+
+# ...............................................................................................
+def simple_stop_loss(data):   
+    data['stop_loss_pip']               = min(data['min_stop_loss_pip'], -data['h_l_gap'] * data['stop_loss_multiplier'])
+
+    if data['open_order']:
+        if data['pl'] <= data['stop_loss_pip']:
+            if data['open_order_type'] == 'long':
+                data['stop_text'] = 'simple_stop'
+                data = close_long_order(data)
+                    
+            if data['open_order_type'] == 'short':                
+                data['stop_text'] = 'simple_stop'
+                data = close_short_order(data)
+    return(data)   
+# ...............................................................................................   
+
+#...............................................................................................
+def simple_take_profit(data):       
+
+    data['pl_move_trail_trigger']       = max(data['min_take_profit_pip'], data['h_l_gap'] * data['take_profit_multiplier'])
+
+    if data['open_order']:                        
+        if data['pl'] >= data['pl_move_trail_trigger']:
+            if data['open_order_type'] == 'long':
+                data['stop_text'] = 'simple_take_profit'
+                data = close_long_order(data)  
+
+            if data['open_order_type'] == 'short':                
+                data['stop_text'] = 'simple_take_profit'
+                data = close_short_order(data)  
+    
+    return(data)    
+#...............................................................................................
+
+#...............................................................................................
+def slema_positive_check(data):
+    if data['slema_check_flag']:
+        if data['open_order']:
+            if data['open_order_type'] == 'long':
+                if data['sema'] > data['lema']:
+                    data['slema_positive'] = True
+                    data['slema_check_flag'] = False
+                else:
+                    data['slema_positive'] = False
+
+            if data['open_order_type'] == 'short':
+                if data['sema'] < data['lema']:
+                    data['slema_positive'] = True
+                    data['slema_check_flag'] = False
+                else:
+                    data['slema_positive'] = False
+
+    return(data)
+#...............................................................................................
+
+#...............................................................................................
+
+def simple_slema_move_close(data):
+    if data['open_order']:
+        if data['slema_positive']: 
+            if data['pl'] > 0:            
+
+                if data['open_order_type'] == 'long':
+                    if data['sema'] < data['lema']:
+                        data['stop_text'] = 'simple_slema_move_close'
+                        data = close_long_order(data)               
+            
+                if data['open_order_type'] == 'short':
+                    if data['sema'] > data['lema']:                
+                        data['stop_text'] = 'simple_slema_move_close'
+                        data = close_short_order(data)  
+
+            else:
+                if data['open_order_type'] == 'long':
+                    if data['sema'] < data['lema']:
+                        data['slema_positive'] = False
+                        data['slema_check_flag'] = True
+
+                if data['open_order_type'] == 'short':
+                    if data['sema'] > data['lema']:
+                        data['slema_positive'] = False
+                        data['slema_check_flag'] = True
+
+    return(data)    
+
+#...............................................................................................
+def trail_take_profit(data):
+    data['pl_move_trail_trigger']       = max(data['min_take_profit_pip'], data['h_l_gap'] * data['take_profit_multiplier'])
+
+    if data['open_order']:
+        if data['pl'] >= data['pl_move_trail_trigger']:
+            data['pl_positive'] = True
+            data['pl_move_min'] = max((data['pl'] * data['pl_move_trail_ratio']), data['pl_move_min'])
+
+        if data['pl_positive']:    
+            if 0 < data['pl'] <= data['pl_move_min']: 
+                if data['open_order_type'] == 'long':
+                    data['stop_text'] = 'pl_move_close'
+                    data['to_order'] = None
+                    data['pl_positive'] = False
+                    data['pl_move_min'] = 0
+                    data = close_long_order(data)             
+                    
+                if data['open_order_type'] == 'short':
+                    data['stop_text'] = 'pl_move_close'
+                    data['to_order'] = None
+                    data['pl_positive'] = False
+                    data['pl_move_min'] = 0
+                    data = close_short_order(data)
+
+    return(data)    
+#...............................................................................................
+
+
+#...............................................................................................
+def lock_profit(data):
+    data['safety_pip_level']       = -data['h_l_gap'] * 0.5
+
+    if data['open_order']:
+        if data['pl'] <= data['safety_pip_level']:
+            data['pl_safety_reached'] = True
+
+        if data['pl_safety_reached']:    
+            if data['pl'] > 0: 
+                if data['open_order_type'] == 'long':
+                    data['stop_text'] = 'profit_lock_close'
+                    data['to_order'] = None
+                    data['pl_positive'] = False
+                    data['pl_safety_reached'] = False
+                    data = close_long_order(data)             
+                    
+                if data['open_order_type'] == 'short':
+                    data['stop_text'] = 'profit_lock_close'
+                    data['to_order'] = None
+                    data['pl_positive'] = False
+                    data['pl_safety_reached'] = False
+                    data = close_short_order(data)
+
+    return(data)    
+#...............................................................................................
+
+
+#...............................................................................................
+def make_long_order(data):
+    data['order_ask_price'] = data['ask']
+    data['open_order'] = True
+    data['slema_check_flag'] = True
+    data['open_order_type'] = 'long'
+    data['pl_positive'] = False
+    data['pl_safety_reached'] = False
+    data['pl_move_min'] = 0
+    data['reverse'] = None
+    data['df']['order_side'].iloc[data['i']]    = 'long'
+    data['df']['long_open'].iloc[data['i']]     = data['ask']
+    data['ordered_touched_line']                = data['touched_line']
+    data['df']['touched_line'].iloc[data['i']]  = data['ordered_touched_line']
+    return(data)
+
+
+def make_short_order(data):
+    data['order_bid_price'] = data['bid']
+    data['open_order'] = True
+    data['slema_check_flag'] = True
+    data['open_order_type'] = 'short'
+    data['pl_positive'] = False
+    data['pl_safety_reached'] = False
+    data['pl_move_min'] = 0
+    data['reverse'] = None
+    data['df']['order_side'].iloc[data['i']] = 'short'
+    data['df']['short_open'].iloc[data['i']] = data['bid']
+    data['ordered_touched_line']                = data['touched_line']
+    data['df']['touched_line'].iloc[data['i']] = data['ordered_touched_line']
+    return(data)
+
+
+def close_long_order(data):
+    data['open_order'] = False
+    data['pl_positive'] = False
+    data['pl_safety_reached'] = False
+    data['pl_move_min'] = 0
+    data['reverse'] = None
+    data['df']['close_type'].iloc[data['i']] = data['stop_text']
+    data['df']['long_close'].iloc[data['i']] = data['bid']  
+    data['df']['pl'].iloc[data['i']] = data['pl']
+    data['df']['order_side'].iloc[data['i']] = 'long'
+    data['df']['touched_line'].iloc[data['i']]  = data['ordered_touched_line']
+    create_report(data)
+    return(data)
+
+
+def close_short_order(data):
+    data['open_order'] = False
+    data['pl_positive'] = False
+    data['pl_safety_reached'] = False
+    data['pl_move_min'] = 0
+    data['df']['close_type'].iloc[data['i']] = data['stop_text']
+    data['df']['short_close'].iloc[data['i']] = data['ask']  
+    data['df']['pl'].iloc[data['i']] = data['pl']
+    data['df']['order_side'].iloc[data['i']] = 'short'
+    data['df']['touched_line'].iloc[data['i']]  = data['ordered_touched_line']
+    data['reverse'] = None
+    create_report(data)
+    return(data)
+
+
+#...............................................................................................
+#...............................................................................................
+#...............................................................................................
+#...............................................................................................
+#...............................................................................................
+#...............................................................................................
+#...............................................................................................
+#...............................................................................................
---
+++ bt_angle_multi/utils/variables.py
@@ -0,0 +1,225 @@
+from utils.packages import *
+
+
+#...............................................................................................
+data = {}
+
+data['df'] = pd.DataFrame()
+data['total_df'] = pd.DataFrame()
+data['dt_list'] =  collections.deque()
+data['start_dt_list'] =  collections.deque()
+data['dir_list'] = collections.deque()
+data['after_order_dir_list'] = collections.deque()
+data['sema_list'] = collections.deque()
+data['lema_list'] = collections.deque()
+data['slema_list'] = collections.deque()
+data['llema_list'] = collections.deque()
+data['pl_list'] =  collections.deque()
+data['start_price'] =  collections.deque()
+data['end_price'] =  collections.deque()
+data['num_orders'] =  collections.deque()
+data['sema_angle_list'] =  collections.deque()
+data['lema_angle_list'] =  collections.deque()
+data['llema_angle_list'] =  collections.deque()
+
+
+data['i_list'] = []
+data['tick_list'] = []
+data['angle_list'] = []
+data['df_sema_list'] = []
+data['df_lema_list'] = []
+data['df_slema_list'] = []
+data['df_llema_list'] = []
+data['df_tick_list'] = []
+data['df_sema_angle_list'] = []
+data['df_lema_angle_list'] = []
+data['df_llema_angle_list'] = []
+
+
+data['buy_markers_x'] = []
+data['buy_markers_y'] = []
+data['sell_markers_x'] = []
+data['sell_markers_y'] = []
+
+data['position'] = False
+data['after_order_position'] = False
+data['open_order'] = 0    
+data['open_order_type'] = None
+data['pl_positive'] = False
+data['pl_positive_flag'] = True
+data['slema_positive'] = False
+data['tick_positive'] = False
+data['slema_check_flag'] = False
+data['tick_check_flag'] = False
+data['tick_close_flag'] = False
+data['tick_close_flag'] = False
+data['pl_available'] = False
+data['stop_loss_flag'] = True
+data['reverse_order_flag'] =  None
+data['reverse'] =  None
+data['slema_closed_flag'] = None
+# data['slema_move_close_flag'] =  None
+
+data['position_without_cushion'] = None
+
+data['orders_list'] = {}
+
+data['close_type'] = []
+data['ord_types'] = []
+data['ll_angle'] = []
+
+# data['running_in'] = 'linux'
+
+data['sema_make_order_angle'] = 1
+data['sema_close_order_angle'] = 1
+data['direction_flag'] = -1
+
+data['pip_size'] = 0.0001
+
+
+data['tick_order_angle'] = 20
+data['stop_loss_pip'] = 0.0010
+
+data['min_order_angle'] = 2.5
+data['position'] = None
+data['pip_decimal_num'] = 10**-6
+data['test_val'] = []
+
+data['date_index'] = 0
+
+data['close_angle'] = 40
+data['lema_close_angle'] = 10
+data['angle_close_pip'] = 0.0002
+
+data['gap_cushion'] = data['pip_size'] * 2
+data['sema_gap_pip'] = 0.0015
+
+# data['pl_move_trigger'] = 0.0003
+# data['pl_min']            = 0.0010
+
+
+data['negative_hit_limit']      = False
+data['pl_negative']             = False
+data['pl_loss_min']             = -100
+data['pl_move_min']             = 0
+
+data['close_angle']     = 25
+data['tick_close_angle'] = 5
+data['pl_close_angle'] = 15
+
+data['dir_change'] = False
+data['to_order'] = None
+
+data['min_llema_angle'] = 0
+
+data['take_profit_method']  = 'simple'
+data['stop_loss_method']    = 'simple'
+
+data['pl_loss_trail_trigger']       = -0.0020
+data['pl_loss_trail_size']          = 1.25
+
+
+
+data['input_rows'] = None
+data['sema_close_flag'] = False
+
+# --------------------------------------------------------
+data['reversed'] = False
+data['order_num_i'] = 0
+
+
+data['min_hl_gap'] = 0.0003
+data['min_take_profit_pip'] = 0.00005
+data['min_stop_loss_pip']   = -0.0003
+
+data['num_of_switch_orders']    = 6
+data['loss_switch_pl_pip']      = -0.0020
+data['all_close_min_pip']       = 0
+data['order_multiplier']        = 1
+
+data["product"] = 'eurusd'
+data["input_year"] = 2021
+data["input_file_name"] = f'{data["product"]}_{data["input_year"]}.csv'
+
+data['df_subset_size'] = None
+
+data['start_date'] = {'year':2021, 'month':1, 'date':1}
+data['end_date']   = {'year':2021, 'month':1, 'date':11}
+
+data['ema_roll_method'] = 'file'
+
+# data['sema_len']    = 1000
+# data['slema_len']   = 5000
+# data['lema_len']    = 10000
+# data['angle_len']   = 10000
+
+x                   = 1000
+data['sema_len']    = int(x / 5)
+data['slema_len']   = int(x)
+data['lema_len']    = int(x * 2)
+data['angle_len']   = int(x * 2)
+# data['angle_len_2']   = int(x * 2)
+data['candle_size'] = x
+data['avg_candle_num'] = 2
+
+
+data['stop_loss_multiplier']    = 0.5
+data['take_profit_multiplier']  = 0.5
+data['pl_move_trail_ratio']     = 0.8
+
+# data['get_cross_dir_type'] = 'multi_row_rl'
+# data['rl_rows'] = 4
+# data['get_cross_dir_type'] = 'normal'
+# data['get_cross_dir_type'] = 'simple_rl'
+data['get_cross_dir_type'] = 'angle_alone'
+
+data["plot"] = True
+data['plot_type'] = 'file'
+data['reduce_plot'] = True
+data['plot_start']  = '2021-01-1'
+data['plot_stop']   = '2021-01-10'
+# data['things_to_plot'] = ['tick', 'lema', 'h_l_lema', 'tick_angle', 'positions']
+data['things_to_plot'] = ['tick', 'ema', 'h_l_lema', 'tick_angle', 'positions']
+
+# data['direction'] = 'reverse'
+data['direction'] = 'straight'
+
+# data['columns_list'] = ['DateTime_frmt', 'Bid', 'Ask', 'tick', 'lema', 'tick_angle', 'h_l_gap', 'h_lema', 'l_lema']
+# data['columns_list'] = ['DateTime_frmt', 'Bid', 'Ask', 'tick', 'lema', 'h', 'l', 'h_l_gap', 'h_lema', 'l_lema']
+data['columns_list']        = ['DateTime_frmt', 'Bid', 'Ask', 'tick', 'lema', 'tick_angle', 'h_l_gap', 'h_lema', 'l_lema']
+data['final_columns_list']  = ['DateTime_frmt', 'month_val', 'date_val', 'hour_val','Bid', 'Ask', 'tick', 'lema', 'tick_angle', 'h_l_gap', 'h_lema', 'l_lema', 'long_open', 'short_open', 'long_profit','long_loss', 'short_profit', 'short_loss','close_type', 'pl']
+
+data['columns_list']        = ['DateTime_frmt', 'Bid', 'Ask', 'tick', 'lema', 'tick_angle', 'h_l_gap', 'h_lema', 'l_lema', 'h_lema_2', 'l_lema_2']
+data['final_columns_list']  = ['DateTime_frmt', 'month_val', 'date_val', 'hour_val','Bid', 'Ask', 'tick', 'lema', 'tick_angle', 'h_l_gap', 'h_lema', 'l_lema', 'h_lema_2', 'l_lema_2', 'long_open', 'short_open', 'long_profit','long_loss', 'short_profit', 'short_loss','close_type', 'pl']
+
+
+data['to_csv']      = True
+
+data['take_profit_flag'] = False
+
+data['short_start'] = False
+data['long_start'] = False
+data['delay_counter'] = 0
+data['delay_tics_num'] = 100
+
+data['candle_size_2'] = 2000
+data['marker_size'] = 1
+
+data['positions_half_closed'] = False
+
+data['temp_df'] = pd.DataFrame()
+data['open_order_temp_list']   = []
+data['pl_temp_list']           = []
+data['first_type']             = None
+data['forward_order_list'] = []
+
+data["csv_file_name"] = 'ema_df-(2021-2021)-(1-1)-(1-10)'
+
+data['csv_list'] = ['ema_2021_jan_aug' , 'ema_df-(2021-2021)-(9-9)-(1-30)', 'ema_df-(2021-2021)-(10-10)-(1-31)', 'ema_df-(2021-2021)-(12-12)-(1-31)']
+data['new_file'] = 'ema_2021-jan_dec'
+
+data['chrome_path'] = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+
+data['temp_total_pl'] = []
+data['send_message_to_phone'] = True
+#...............................................................................................
