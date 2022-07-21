0bdcd41601ee58ce14b2f9e82e76624a74b5749d
Ramkumar Rajagopalan <ramrajag6@gmail.com>
2022-07-21 14:24:44 +0800

    latest commit

---
+++ bt_candle_patterns/aws_pack/utils/dir_slope.py
@@ -0,0 +1,44 @@
+from utils.packages import *
+
+
+#...............................................................................................
+def get_position(data):
+    
+    if data['tick'] >= data['h_lema']:
+        data['position'] = -1
+
+    elif data['tick'] <= data['l_lema']:
+        data['position'] = 1
+    
+    else:
+        data['position'] = 0
+
+    return(data)
+#...............................................................................................
+
+
+#...............................................................................................
+def get_cross_dir(data):   
+    
+    data['dir_list'].popleft()
+    data['dir_list'].append(data['position'])   
+    
+    data['pos_1'] = data['dir_list'][0]
+    data['pos_2'] = data['dir_list'][1]
+
+    if data['pos_1'] != data['pos_2'] and data['pos_2'] == -1:
+        if data['h_l_gap'] > data['min_hl_gap']:
+            data['dir_change'] = True
+            data['to_order'] = 'short'      
+
+    elif data['pos_1'] != data['pos_2'] and data['pos_2'] == 1:
+        if data['h_l_gap'] > data['min_hl_gap']:
+            data['dir_change'] = True    
+            data['to_order'] = 'long'
+        
+    else:
+        data['dir_change'] = False
+        data['to_order'] = None
+
+    return(data)    
+#................................................................................................
---
+++ bt_candle_patterns/utils/packages.py
@@ -0,0 +1,35 @@
+#...............................................................................................
+import os
+import sys
+import time
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+import requests
+from config import keys
+
+import math
+from scipy.stats import linregress
+
+import collections
+
+from tqdm import tqdm
+from IPython import display
+from tqdm import tqdm
+tqdm.pandas()
+
+import warnings
+warnings.filterwarnings('ignore')
+
+# -------------------------------------------------------------------------------
+
+import winsound
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.graph_objs.layout import YAxis,XAxis,Margin
+import webbrowser
+#...............................................................................................
